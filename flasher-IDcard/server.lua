44c4dfd0833a353f88d0e3744211b51dee7c9056
Devflasher293 <75735872+Devflasher293@users.noreply.github.com>
2020-12-11 00:30:11 +0100

    Removed pNotify, uppgraded the system a bit.

--- flasher-IDcard/server.lua
+++ flasher-IDcard/server.lua
@@ -8,13 +8,21 @@ RegisterNetEvent("flasher-ID-Card:GiveID")
 AddEventHandler("flasher-ID-Card:GiveID", function()
 
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local price = 100 
+	local price = 500 
 	local item = 'idcard'
 	local itemCount = xPlayer.getInventoryItem(item).count
-		
+
+	if itemCount == 1 then
+		TriggerClientEvent("esx:showNotification", source, "Du har redan ett legitimation på dig!")
+	end
+	if xPlayer.getMoney() <= price 
+	then
+		TriggerClientEvent("esx:showNotification", source, "Du har inte råd att hämta ut din legitimaion")
+	end
+
 	if itemCount <= 0 then 
 		if xPlayer.getMoney() >= price then 
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Du har tagit emot ditt ID-Kort.', length = 2500, style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } })
+			TriggerClientEvent("esx:showNotification", source, "Du hämtade ut din legitimation och betalade <span style='color:green; font-weight: bold'>500</span> SEK.")
 			xPlayer.removeMoney(price)
 			xPlayer.addInventoryItem(item, 1)
 		end
